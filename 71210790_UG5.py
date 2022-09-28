4aaf185b17bcbd0c51540cbb65a8ee2dff5cebc0
GustyFachtaPratama <112554244+GustyFachtaPratama@users.noreply.github.com>
2022-09-28 19:18:59 +0700

    Add files via upload

--- 71210790_UG5.py
+++ 71210790_UG5.py
@@ -1,49 +1,58 @@
-from pickle import NONE
-
-
 class Karyawan:
-    _nama = ""
-    _umur = ""
-    _jenisKelamin = NONE
-    _upahPerHari = NONE
+       _nama = ""
+       _umur = 0
+       _jenisKelamin = ""
+       _upahPerHari = 0
 
-    def __init__(self, nama, umur, jenisKelamin, upahPerHari):
-        self._nama = nama
-        self._umur = umur
-        self._jenisKelamin = jenisKelamin
-        self._upahPerHari = upahPerHari
+       def __init__(self, nama, umur):
+              self._nama = nama
+              self._umur = umur
+              self._jenisKelamin = None
+              self._upahPerHari = None
 
-    def setNama(self,nama):
-        self._nama = nama
+       def setNama(self, nama):
+              self._nama = nama
 
-    def setUmur(self,umur):
-        self._umur = umur
+       def setUmur(self, umur):
+              self._umur = umur
 
-    def setJenisKelamin(self,jenisKelamin):
-        self._jenisKelamin = jenisKelamin
+       def setJenisKelamin(self, jenisKelamin):
+              self._jenisKelamin = jenisKelamin
 
-    def setUpahPerHari(self,upahPerhari):
-        self._upahPerHari = upahPerhari
+       def setUpahPerHari(self, upahPerHari):
+              self._upahPerHari = upahPerHari
 
-    def printInfo(self):
-        print("Nama :", self._nama)
-        print("Umur :", self._umur)
-        print("Jenis Kelamin :", self._jenisKelamin)
-        print("Upah Perhari :", self._upahPerHari)
+       def getNama(self):
+              return self._nama
 
-    def hitungGajiBulanan(self,tiapHari):
-        if self._upahPerHari  == None  or self._jenisKelamin == None:
-            print ("EROR! Kapasitas BBM atau Jenis Bahan Bakar belum diinputkan")
-        else :   
-            print("Mengisi :",self.setUpahPerHari())
-            print("Total Harga :",self.setUpahPerHari()*tiapHari)
+       def getUmur(self):
+              return self._umur
+       
+       def getJenisKelamin(self):
+              return self._jenisKelamin
+       
+       def getUpahPerHari(self):
+              return self._upahPerHari
+
+       def printInfo(self):
+              print("="*11,"INFO","="*11)
+              print("Nama          :", self.getNama())
+              print("Umur          :", self.getUmur())
+              print("Jenis Kelamin :", self.getJenisKelamin())
+              print("Upah per Hari :", self.getUpahPerHari())
+
+       def hitungGajiBulanan(self, jumlahHari):
+              if self.getUpahPerHari() == None:
+                     print("ERROR! Upah per Hari belum di inputkan")
+              else:
+                     print("Gaji Bulan ini :",str( self.getUpahPerHari()*jumlahHari))
 
 if __name__ == "__main__":
-    orang1 = Karyawan("Haniif", 90,None,None)
-    orang1.printInfo()
-    orang2 = Karyawan("Sindu", 190,None,None)
-    orang2.setJenisKelamin("Laki-laki")
-    orang2.setUpahPerHari(30000)
-    orang2.printInfo()
-    orang1.hitungGajiBulanan(28)
-    orang2.hitungGajiBulanan(30)
+       orang1 = Karyawan("Haniif", 90)
+       orang1.printInfo()
+       orang2 = Karyawan("Sindu", 190)
+       orang2.setJenisKelamin("Laki-laki")
+       orang2.setUpahPerHari(30000)
+       orang2.printInfo()
+       orang1.hitungGajiBulanan(28)
+       orang2.hitungGajiBulanan(30)
